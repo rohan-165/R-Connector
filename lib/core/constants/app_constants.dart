70c43d64c00e690b1e742958d378aff5de5131c6
Roh-An <rohanchy165@gmail.com>
2025-10-13 17:36:32 +0545

    [CHORE] chore: update app constants and remove unused question type constants
    - Changed app name from 'MyApp' to 'DRI VCTRS'.
    - Removed unused constants from QuestionTypesConstants and QuestionFormat classes.

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -3,30 +3,7 @@
 class AppConstants {
   AppConstants._();
 
-  static const String appName = 'MyApp';
-}
-
-class QuestionTypesConstants {
-  QuestionTypesConstants._();
-
-  static const String time = "input_time";
-  static const String dropdown = "input_spinner";
-  static const String freeText = "input_edittext";
-  static const String date = "input_date";
-  static const String contactNo = "contact_no";
-  static const String radioButtonImage = "input_radio_image";
-  static const String radioButton = "input_radio";
-  static const String ageInputEdittext = "age_input_edittext";
-  static const String multiSelect = "input_checkbox";
-  static const String image = "input_image";
-  static const String autoSelector = "auto_selector";
-}
-
-class QuestionFormat {
-  QuestionFormat._();
-
-  static const String header = 'header';
-  static const String normal = 'normal';
+  static const String appName = 'DRI VCTRS';
 }
 
 class DatePickerType {
@@ -39,8 +16,6 @@ class NavBarConstants {
   NavBarConstants._();
 
   static const String home = 'Home';
-  static const String startSurvey = 'Start_Survey';
-  static const String notifications = 'Notifications';
   static const String profile = 'Profile';
 }
 
