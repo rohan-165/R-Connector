29f3e78c3fc7f8e07fd3b48aaa7820b5c95287b8
Roh-An <rohanchy165@gmail.com>
2025-10-16 17:47:04 +0545

    [CHORE] **Commit Message:**
    ```
    chore: Refactor SignCubit and SignFormScreen to enhance navigation and maintainability
    
    - Added NavigationService to SignCubit for improved navigation handling.
    - Simplified DropdownWidget usage in SignFormScreen with a more structured layout.
    - Enhanced form validation messages for clarity.
    - Updated sign action button to reflect loading state.
    ```

--- lib/features/dashbord/presentation/cubit/sign_cubit.dart
+++ lib/features/dashbord/presentation/cubit/sign_cubit.dart
@@ -3,6 +3,7 @@ import 'package:injectable/injectable.dart';
 import 'package:r_connector/core/common/abs_normal_state.dart';
 import 'package:r_connector/core/constants/enum.dart';
 import 'package:r_connector/core/services/get_it/service_locator.dart';
+import 'package:r_connector/core/services/navigation_service.dart';
 import 'package:r_connector/features/dashbord/domain/repo/repo.dart';
 
 @lazySingleton
@@ -37,7 +38,11 @@ class SignCubit extends Cubit<AbsNormalState> {
     );
 
     resp.fold(
-      (l) => emit(state.copyWith(absNormalStatus: AbsNormalStatus.SUCCESS)),
+      (l) {
+        emit(state.copyWith(absNormalStatus: AbsNormalStatus.SUCCESS));
+        getIt<NavigationService>().safePop();
+        getIt<NavigationService>().safePop();
+      },
       (r) => emit(
         state.copyWith(absNormalStatus: AbsNormalStatus.ERROR, failure: r),
       ),
--- lib/features/dashbord/presentation/screen/sign_form_screen.dart
+++ lib/features/dashbord/presentation/screen/sign_form_screen.dart
@@ -1,6 +1,12 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:r_connector/core/common/abs_normal_state.dart';
 import 'package:r_connector/core/constants/app_constants.dart';
+import 'package:r_connector/core/constants/enum.dart';
 import 'package:r_connector/core/extension/widget_extensions.dart';
+import 'package:r_connector/core/services/get_it/service_locator.dart';
+import 'package:r_connector/features/dashbord/presentation/cubit/pdf_cubit.dart';
+import 'package:r_connector/features/dashbord/presentation/cubit/sign_cubit.dart';
 import 'package:r_connector/widget/app_bar_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:r_connector/widget/button_widget.dart';
@@ -53,178 +59,203 @@ class _SignFormScreenState extends State<SignFormScreen> {
       appBar: AppBarWidget(showBackButton: true, title: 'Sign Form'),
       body: Form(
         key: _formKey,
-        child: Column(
-          children: [
-            ValueListenableBuilder(
-              valueListenable: signPage,
-              builder: (_, value, __) {
-                return DropdownWidget<String>(
-                  showSearchBox: false,
-                  lable: 'Select Page',
-                  selectedItem: value.isNotEmpty ? value : null,
-                  items: (filter, loadProps) => _signPage,
-                  validator: (v) {
-                    if ((v ?? '').isEmpty || v == null) {
-                      return 'Please select Value';
-                    } else {
-                      return null;
-                    }
-                  },
-                  onChanged: (value) {
-                    signPage.value = value ?? '';
-                  },
-                ).padBottom(bottom: 10.h);
-              },
-            ),
-            ValueListenableBuilder(
-              valueListenable: certificateFileSource,
-              builder: (_, value, __) {
-                return DropdownWidget<String>(
-                  showSearchBox: false,
-                  lable: 'File Source Certificate',
-                  selectedItem: value.isNotEmpty ? value : null,
-                  items: (filter, loadProps) => _fileCertificatType,
-                  validator: (v) {
-                    if ((v ?? '').isEmpty || v == null) {
-                      return 'Please select Value';
-                    } else {
-                      return null;
-                    }
-                  },
-                  onChanged: (value) {
-                    certificateFileSource.value = value ?? '';
-                  },
-                ).padBottom(bottom: 10.h);
-              },
-            ),
-            ValueListenableBuilder(
-              valueListenable: qrImageSource,
-              builder: (_, value, __) {
-                return DropdownWidget<String>(
-                  showSearchBox: false,
-                  lable: 'QR Image Source Certificate',
-                  selectedItem: value.isNotEmpty ? value : null,
-                  items: (filter, loadProps) => _fileCertificatType,
-                  validator: (v) {
-                    if ((v ?? '').isEmpty || v == null) {
-                      return 'Please select Value';
-                    } else {
-                      return null;
-                    }
-                  },
-                  onChanged: (value) {
-                    qrImageSource.value = value ?? '';
-                  },
-                ).padBottom(bottom: 10.h);
-              },
-            ),
-            ValueListenableBuilder(
-              valueListenable: unsignedSource,
-              builder: (_, value, __) {
-                return DropdownWidget<String>(
-                  showSearchBox: false,
-                  lable: 'Unsigned Source Certificate',
-                  selectedItem: value.isNotEmpty ? value : null,
-                  items: (filter, loadProps) => _fileCertificatType,
-                  validator: (v) {
-                    if ((v ?? '').isEmpty || v == null) {
-                      return 'Please select Value';
-                    } else {
-                      return null;
-                    }
-                  },
-                  onChanged: (value) {
-                    unsignedSource.value = value ?? '';
-                  },
-                ).padBottom(bottom: 10.h);
-              },
-            ),
-            ValueListenableBuilder(
-              valueListenable: stampSource,
-              builder: (_, value, __) {
-                return DropdownWidget<String>(
-                  showSearchBox: false,
-                  lable: 'Stamp Source Certificate',
-                  selectedItem: value.isNotEmpty ? value : null,
-                  items: (filter, loadProps) => _fileCertificatType,
-                  validator: (v) {
-                    if ((v ?? '').isEmpty || v == null) {
-                      return 'Please select Value';
-                    } else {
-                      return null;
-                    }
-                  },
-                  onChanged: (value) {
-                    stampSource.value = value ?? '';
-                  },
-                ).padBottom(bottom: 10.h);
-              },
-            ),
-            ValueListenableBuilder(
-              valueListenable: publicCertificateFileSource,
-              builder: (_, value, __) {
-                return DropdownWidget<String>(
-                  showSearchBox: false,
-                  lable: 'Public File Source Certificate',
-                  selectedItem: value.isNotEmpty ? value : null,
-                  items: (filter, loadProps) => _fileCertificatType,
-                  validator: (v) {
-                    if ((v ?? '').isEmpty || v == null) {
-                      return 'Please select Value';
-                    } else {
-                      return null;
-                    }
-                  },
-                  onChanged: (value) {
-                    publicCertificateFileSource.value = value ?? '';
-                  },
-                ).padBottom(bottom: 10.h);
-              },
-            ),
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(
+            parent: AlwaysScrollableScrollPhysics(),
+          ),
+          child: Column(
+            children: [
+              ValueListenableBuilder(
+                valueListenable: signPage,
+                builder: (_, value, __) {
+                  return DropdownWidget<String>(
+                    showSearchBox: false,
+                    lable: 'Select Page',
+                    selectedItem: value.isNotEmpty ? value : null,
+                    items: (filter, loadProps) => _signPage,
+                    validator: (v) {
+                      if ((v ?? '').isEmpty || v == null) {
+                        return 'Please select Value';
+                      } else {
+                        return null;
+                      }
+                    },
+                    onChanged: (value) {
+                      signPage.value = value ?? '';
+                    },
+                  ).padBottom(bottom: 10.h);
+                },
+              ),
+              ValueListenableBuilder(
+                valueListenable: certificateFileSource,
+                builder: (_, value, __) {
+                  return DropdownWidget<String>(
+                    showSearchBox: false,
+                    lable: 'File Source Certificate',
+                    selectedItem: value.isNotEmpty ? value : null,
+                    items: (filter, loadProps) => _fileCertificatType,
+                    validator: (v) {
+                      if ((v ?? '').isEmpty || v == null) {
+                        return 'Please select Value';
+                      } else {
+                        return null;
+                      }
+                    },
+                    onChanged: (value) {
+                      certificateFileSource.value = value ?? '';
+                    },
+                  ).padBottom(bottom: 10.h);
+                },
+              ),
+              ValueListenableBuilder(
+                valueListenable: qrImageSource,
+                builder: (_, value, __) {
+                  return DropdownWidget<String>(
+                    showSearchBox: false,
+                    lable: 'QR Image Source Certificate',
+                    selectedItem: value.isNotEmpty ? value : null,
+                    items: (filter, loadProps) => _fileCertificatType,
+                    validator: (v) {
+                      if ((v ?? '').isEmpty || v == null) {
+                        return 'Please select Value';
+                      } else {
+                        return null;
+                      }
+                    },
+                    onChanged: (value) {
+                      qrImageSource.value = value ?? '';
+                    },
+                  ).padBottom(bottom: 10.h);
+                },
+              ),
+              ValueListenableBuilder(
+                valueListenable: unsignedSource,
+                builder: (_, value, __) {
+                  return DropdownWidget<String>(
+                    showSearchBox: false,
+                    lable: 'Unsigned Source Certificate',
+                    selectedItem: value.isNotEmpty ? value : null,
+                    items: (filter, loadProps) => _fileCertificatType,
+                    validator: (v) {
+                      if ((v ?? '').isEmpty || v == null) {
+                        return 'Please select Value';
+                      } else {
+                        return null;
+                      }
+                    },
+                    onChanged: (value) {
+                      unsignedSource.value = value ?? '';
+                    },
+                  ).padBottom(bottom: 10.h);
+                },
+              ),
+              ValueListenableBuilder(
+                valueListenable: stampSource,
+                builder: (_, value, __) {
+                  return DropdownWidget<String>(
+                    showSearchBox: false,
+                    lable: 'Stamp Source Certificate',
+                    selectedItem: value.isNotEmpty ? value : null,
+                    items: (filter, loadProps) => _fileCertificatType,
+                    validator: (v) {
+                      if ((v ?? '').isEmpty || v == null) {
+                        return 'Please select Value';
+                      } else {
+                        return null;
+                      }
+                    },
+                    onChanged: (value) {
+                      stampSource.value = value ?? '';
+                    },
+                  ).padBottom(bottom: 10.h);
+                },
+              ),
+              ValueListenableBuilder(
+                valueListenable: publicCertificateFileSource,
+                builder: (_, value, __) {
+                  return DropdownWidget<String>(
+                    showSearchBox: false,
+                    lable: 'Public File Source Certificate',
+                    selectedItem: value.isNotEmpty ? value : null,
+                    items: (filter, loadProps) => _fileCertificatType,
+                    validator: (v) {
+                      if ((v ?? '').isEmpty || v == null) {
+                        return 'Please select Value';
+                      } else {
+                        return null;
+                      }
+                    },
+                    onChanged: (value) {
+                      publicCertificateFileSource.value = value ?? '';
+                    },
+                  ).padBottom(bottom: 10.h);
+                },
+              ),
 
-            ValueListenableBuilder(
-              valueListenable: stampRoText,
-              builder: (_, value, __) {
-                return DropdownWidget<String>(
-                  showSearchBox: false,
-                  lable: 'Stamp / Text',
-                  selectedItem: value.isNotEmpty ? value : null,
-                  items: (filter, loadProps) => _stamporTexxt,
-                  validator: (v) {
-                    if ((v ?? '').isEmpty || v == null) {
-                      return 'Please select Value';
-                    } else {
-                      return null;
-                    }
-                  },
-                  onChanged: (value) {
-                    signPage.value = value ?? '';
-                  },
-                ).padBottom(bottom: 10.h);
-              },
-            ),
-            TextFieldWidget(
-              labelText: 'Certificate Password',
-              hintText: 'Enter Password',
-              controller: _password,
-              validator: (value) {
-                if ((value ?? '').isEmpty || value == null) {
-                  return 'Please enter password';
-                } else {
-                  return null;
-                }
-              },
-              keyboardType: TextInputType.text,
-            ).padBottom(),
-            ButtonWidget(
-              onTap: () {
-                if (_formKey.currentState!.validate()) {}
-              },
-              lable: 'Sign',
-              horizontal: 0.w,
-            ),
-          ],
-        ).padAll(),
+              ValueListenableBuilder(
+                valueListenable: stampRoText,
+                builder: (_, value, __) {
+                  return DropdownWidget<String>(
+                    showSearchBox: false,
+                    lable: 'Stamp / Text',
+                    selectedItem: value.isNotEmpty ? value : null,
+                    items: (filter, loadProps) => _stamporTexxt,
+                    validator: (v) {
+                      if ((v ?? '').isEmpty || v == null) {
+                        return 'Please select Value';
+                      } else {
+                        return null;
+                      }
+                    },
+                    onChanged: (value) {
+                      signPage.value = value ?? '';
+                    },
+                  ).padBottom(bottom: 10.h);
+                },
+              ),
+              TextFieldWidget(
+                labelText: 'Certificate Password',
+                hintText: 'Enter Password',
+                controller: _password,
+                validator: (value) {
+                  if ((value ?? '').isEmpty || value == null) {
+                    return 'Please enter password';
+                  } else {
+                    return null;
+                  }
+                },
+                keyboardType: TextInputType.text,
+              ).padBottom(),
+              BlocBuilder<SignCubit, AbsNormalState>(
+                builder: (context, state) {
+                  return ButtonWidget(
+                    isLoading: state.absNormalStatus == AbsNormalStatus.LOADING,
+                    onTap: () {
+                      if (_formKey.currentState!.validate()) {
+                        final filePath =
+                            getIt<PdfCubit>().state.data?.fileName ?? '';
+                        getIt<SignCubit>().signPost(
+                          filePath: filePath,
+                          signPage: signPage.value,
+                          certificateFileSource: certificateFileSource.value,
+                          stampSource: stampSource.value,
+                          qrImageSource: qrImageSource.value,
+                          unsignedSource: unsignedSource.value,
+                          publicCertificateFileSource:
+                              publicCertificateFileSource.value,
+                          stampRoText: stampRoText.value,
+                          password: _password.text.trim(),
+                        );
+                      }
+                    },
+                    lable: 'Sign',
+                    horizontal: 0.w,
+                  );
+                },
+              ),
+            ],
+          ).padAll(),
+        ),
       ),
     );
   }
